34ba9be261d0257dab35596573a7eef5e066496d
DELE <mac@Deles-MacBook-Pro.local>
2025-06-27 00:40:21 +0100

    Add TestController with simple test endpoint

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -7,6 +7,7 @@ public class TestController : ControllerBase
     [HttpGet]
     public string Get()
     {
+        //http://localhost:5130/swagger/index.html
         return "API is working!";
     }
 }
